ba225670de349e2f6060e19ffbdc202083246504
vq0001 <vquiroga2023@udec.cl>
2025-06-16 22:00:36 -0400

    Crear main para probar los algoritmos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Estructura-Datos-Deteccion-Patrones
 Proyecto semestral para el ramo Estructuras de Datos.
+
+### Compilar programa: 
+`g++ main.cpp utils.cpp -Ialgoritmos -o detectar_patrones.exe`
---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include "algoritmos/boyer_moore.cpp"
+#include "utils.hpp"
+#include <filesystem>
+#include <iterator>
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    if (argc < 4) {
+        cerr << "Uso: " << argv[0] << " <algoritmo> <CarpetaConArchivosEntrada> <Patron> <CantidadArchivos>" << endl;
+        return 1;
+    }
+
+     // Obtener el nombre del algoritmo y el archivo de entrada
+    string algoritmo = argv[1];
+    string carpeta = argv[2];
+    string patron = argv[3];
+    string texto;
+    
+    // Leer el archivo de entrada
+    // Si se especifica una cantidad de archivos, se lee esa cantidad
+    if (argc < 4) {
+        texto = readFile(carpeta, stoi(argv[4]));
+    } else {
+        texto = readFile(carpeta);
+    }
+
+    // Ordenar los datos según el algoritmo especificado
+    if (algoritmo == "Boyer_Moore") {
+        startTimer();
+
+        boyer_moore(texto, patron);
+
+        
+    } else if (algoritmo == "KMP") {
+        startTimer();
+        // Implementar KMP aquí
+    } else if (algoritmo == "Robin-Karp") {
+        startTimer();
+        // Implementar Robin-Karp aquí
+    } else {
+        cerr << "Algoritmo no reconocido. Ingresar alguno de los siguientes: Boyer_Moore, KMP, Robin-Karp" << endl;
+        return 1;
+    }
+    
+    stopTimer();
+    return 0;
+}

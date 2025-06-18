57b1d7e7b997b89c66154c9fa1a4084c53aea34b
vq0001 <vquiroga2023@udec.cl>
2025-06-18 12:09:55 -0400

    Corregir header de readFolder y File

--- utils.hpp
+++ utils.hpp
@@ -6,9 +6,10 @@
 #include <iostream>
 #include <fstream>
 #include <chrono>
-
-std::string readFolder(const std::string &carpeta, int cantidadArchivos = -1);
-std::string readFile(const std::string &archivo);
+using namespace std;
+string readFolder(const string &carpeta, vector<int> *posiciones, int cantidadArchivos = -1);
+string readFile(const string &archivo);
+void encuentros_por_archivo(const string &carpeta, const vector<int> &posiciones, const vector<int> &pos_final_archivos);
 void startTimer();
 void stopTimer();
 

018a0cb86b21c0317e38831f75d964dac4b2b4fc
Bastian Bernal <babv20@gmail.com>
2025-07-06 23:01:49 -0400

    fix: Se reemplazan caracteres no ASCII por caracter ASCII.

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -46,7 +46,12 @@ string readFolder(const string &carpeta, vector<int> *posiciones,bool expresivo 
             return "";
         }
 
-        txt = txt + "$" + string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+        string file_content = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+        // Reemplazar caracteres no ASCII
+        for (char &c : file_content) {
+            if (static_cast<unsigned char>(c) > 127) c = '\x1F';
+        }
+        txt = txt + "$" + file_content;
         file.close();
 
         
@@ -83,6 +88,10 @@ string readFile(const string &archivo,bool expresivo) {
     }
 
     string txt = string(istreambuf_iterator<char>(file), istreambuf_iterator<char>());
+    // Reemplazar caracteres no ASCII
+    for (char &c : txt) {
+        if (static_cast<unsigned char>(c) > 127) c = '\x1F';
+    }
     file.close();
     
     return txt; // Devolver el contenido del archivo como una cadena
